b7782d37c6c257d7f43042ad122d09f8001d9f9d
Tristan <tzick002@ucr.edu>
2019-03-20 21:59:16 -0700

    Add initial increment / decrement bpm functionality

--- src/components/metronome.js
+++ src/components/metronome.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
 
 class Metronome extends Component {
+  constructor(props) {
+    super(props);
+    this.MAXBPM = 360;
+    this.state = {
+      BPM: 75
+    }
+  }
+
+  modifyBPM(offset) {
+    var currentBPM = this.state.BPM;
+    var newBPM = currentBPM + offset;
+    if(newBPM < 1){
+      newBPM = 1;
+    }
+    else if(newBPM > this.MAXBPM){
+      newBPM = this.MAXBPM;
+    }
+    this.setState({BPM: newBPM});
+  }
+
+  incrementBPM() {
+    var offset = 1;
+    this.modifyBPM(offset);
+  }
+
+  decrementBPM() {
+    var offset = -1;
+    this.modifyBPM(offset);
+  }
+
   render() {
     return (
       <div className="Metronome">
-      Metronome
+        <h2>Metronome</h2>
+        <div className="BPM">
+          <h3>BPM</h3>
+          <button onClick={() => this.decrementBPM()}>
+            -
+          </button>
+          {this.state.BPM}
+          <button onClick={() => this.incrementBPM()}>
+            +
+          </button>
+        </div>
       </div>
     );
   }
